b65895fcb460575d850c9274c50dd88f86e016c5
scrapasd <47080903+aslam-c@users.noreply.github.com>
2021-05-11 14:45:19 +0530

    seed files added

---
+++ src/migration/seeds/users.seed.ts
@@ -0,0 +1,44 @@
+export const UsersSeed = [
+  {
+    id: 3000,
+    name: "U1",
+    age: 21,
+    password: "luckyshine001",
+    email: "u1@luckyshine.xyz",
+  },
+  {
+    id: 3001,
+    name: "U2",
+    age: 51,
+    password: "luckyshine002",
+    email: "u2@luckyshine.xyz",
+  },
+  {
+    id: 3002,
+    name: "U3",
+    age: 31,
+    password: "luckyshine003",
+    email: "u1@luckyshine.xyz",
+  },
+  {
+    id: 3003,
+    name: "U4",
+    age: "18",
+    password: "luckyshine004",
+    email: "u2@luckyshine.xyz",
+  },
+  {
+    id: 3004,
+    name: "U5",
+    age: 21,
+    password: "luckyshine005",
+    email: "u1@luckyshine.xyz",
+  },
+  {
+    id: 3005,
+    name: "U6",
+    age: 35,
+    password: "luckyshine006",
+    email: "u2@luckyshine.xyz",
+  },
+];
--- src/models/Treasure.ts
+++ src/models/Treasure.ts
@@ -1,4 +1,12 @@
 import { getRepository } from "typeorm";
+import { getManager } from "typeorm";
 import { Treasure } from "../entity/Treasure";
 
-export function getTreasure(location: any) {}
+export async function getTreasures(location: any, distance: number) {
+  const entityManager = getManager();
+  const treasures = await entityManager.query(
+    `SELECT name,id FROM treasures 
+  WHERE ST_DWithin(the_geom, ST_SetSRID(ST_Point($1, $2), 4326), $3)`,
+    [location.longitude, location.latitude, distance]
+  );
+}
